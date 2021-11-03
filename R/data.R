d796d462f8a4961f62d63870e18110f4f6126620
Amy Fox <aef1004@gmail.com>
2021-11-03 17:11:15 -0500

    update data.R

--- R/data.R
+++ R/data.R
@@ -1,20 +1,14 @@
-#' A FlowSet containing all of the FMO samples and measured data
 #'
-#' Description of data
+#' A dataset containing the cutoff value between negative and positive populations for each marker
 #'
-#' @format A flowSet with 25 experiments (samples) containing all of the measured data:
+#' @format A data frame with 20 rows and 2 variables:
 #' \describe{
-#'   \item{SSC-A}{ numeric: the sidescatter, or complexity, of each cell}
-#'   \item{filename}{ character: the name of the FMO sample from which the data comes}
-#'   \item{IL_10}{ numeric: the MFI expression of IL-10 for each cell}
-#'   \item{TNF}{ numeric: the MFI expression of TNF for each cell}
-#'   \item{CD4}{ numeric: the MFI expression of CD4 for each cell}
-#'   \item{CD69}{ numeric: the MFI expression of CD69 for each cell}
-#'   \item{CD8}{ numeric: the MFI expression of CD8 for each cell}
-#'   ...
+#'   \item{filename}{ character: the name of the marker for each cutoff}
+#'   \item{quantile_99}{ numeric: the corresponding MFI value which is the cutoff between positive and negative populations for a particular marker}
 #' }
-"flowset_FMO_gated_data"
-
+"add_quantile"
+#'
+#'
 #' A FlowSet containing all of the data samples and measured data
 #'
 #' @format A flowSet with 18 experiments (samples) containing all of the measured data:
@@ -29,7 +23,22 @@
 #'   ...
 #' }
 "flowset_gated_data"
-
+#'
+#' A FlowSet containing all of the FMO samples and measured data
+#'
+#' @format A flowSet with 25 experiments (samples) containing all of the measured data:
+#' \describe{
+#'   \item{SSC-A}{ numeric: the sidescatter, or complexity, of each cell}
+#'   \item{filename}{ character: the name of the FMO sample from which the data comes}
+#'   \item{IL_10}{ numeric: the MFI expression of IL-10 for each cell}
+#'   \item{TNF}{ numeric: the MFI expression of TNF for each cell}
+#'   \item{CD4}{ numeric: the MFI expression of CD4 for each cell}
+#'   \item{CD69}{ numeric: the MFI expression of CD69 for each cell}
+#'   \item{CD8}{ numeric: the MFI expression of CD8 for each cell}
+#'   ...
+#' }
+"flowset_FMO_gated_data"
+#'
 #' A dataframe containing all of the samples and measured data
 #'
 #' @format A dataframe containing all of the measured data:
@@ -44,34 +53,7 @@
 #'   ...
 #' }
 "df_FMO_gated_data"
-
-
-
-#' A dataset containing the matched FMO markers to the FMO samples
 #'
-#' @format A data frame with 415,544 rows and 4 variables:
-#' \describe{
-#'   \item{SSC-A}{ numeric: the sidescatter used to plot the FMO data}
-#'   \item{filename}{ character: the name of the FMO sample from which the data comes}
-#'   \item{marker}{ character: the name of the marker of interest that matches up with the name of the FMO sample}
-#'   \item{MFI}{ numeric: mean fluorescent intensity data for each FMO sample in the specific marker channel}
-#'   ...
-#' }
-"FMO_filtered_data"
-
-
-
-#' A dataset containing the cutoff value between negative and positive populations for each marker
-#'
-#' @format A data frame with 20 rows and 2 variables:
-#' \describe{
-#'   \item{filename}{ character: the name of the marker for each cutoff}
-#'   \item{quantile_99}{ numeric: the corresponding MFI value which is the cutoff between positive and negative populations for a particular marker}
-#'   ...
-#' }
-"add_quantile"
-
-
 #' A dataset containing all of the filtered FMO data
 #'
 #' @format A flowSet with 415,544 rows that represent each cell in the FMO samples and 24 variables which show the different markers:
@@ -86,11 +68,8 @@
 #'   \item{BV480-A}{the MFI expression BV480-A channel  for each cell}
 #'   ...
 #' }
-"gated_data"
-
-
-
-
+ "FMO_filtered_data"
+#'
 #' A dataset containing the feature engineered data
 #'
 #' @format A dataframe with 37,143 rows and 22 variables:
@@ -105,9 +84,8 @@
 #'   \item{percentage}{ numeric: the percentage of cells with a particular phenotype listed in the markers}
 #'   ...
 #' }
-"all_fe"
-
-
+ "all_fe"
+#'
 #' A dataset containing all of the filtered FMO data
 #'
 #' @format A flowSet with 1,056,969 rows and 28 variables:
@@ -122,30 +100,4 @@
 #'   \item{BV480-A}{the MFI expression BV480-A channel  for each cell}
 #'   ...
 #' }
-"df_all_gated"
-
-#' A dataset containing the percentages for each population and sample in the data
-#'
-#' @format A dataframe with 594 rows and 3 variables:
-#' \describe{
-#'   \item{population}{ character: the population name of the filtered data}
-#'   \item{filename}{ character: the sample name from which each data point originates}
-#'   \item{percentage}{ numeric: the percentage of cells with a particular phenotype listed in the markers}
-#'   ...
-#' }
-"sample_populations_all_groups"
-
-
-
-#' A dataset containing the filtered population data
-#'
-#' @format A dataframe with 33 rows and 19 variables:
-#' \describe{
-#'   \item{IL_10}{ factor: the positive (1) or negative (0) expression of IL-10 in a cell}
-#'   \item{TNF}{ factor: the positive (1) or negative (0) expression of TNF in a cell}
-#'   \item{CD4}{ factor: the positive (1) or negative (0) expression of CD4 in a cell}
-#'   \item{CD8}{ factor: the positive (1) or negative (0) expression of CD8 in a cell}
-#'   \item{population}{ character: the population name of the filtered data}
-#'   ...
-#' }
-"sample_populations"
+ "df_all_gated"
